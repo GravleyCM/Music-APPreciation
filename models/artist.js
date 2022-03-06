10fab727c080bd465c36b42a89a5270a5520bb7d
GravleyCM <GravleyCM@gmail.com>
2022-03-06 11:24:18 -0500

    update artistSchema

--- models/artist.js
+++ models/artist.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const artistSchema = new Schema ({
-  
+  name: String,
+  genre: String,
+  album: [albumSchema,]
 })
 
 const Artist = mongoose.model("Artist", artistSchema)
