a22856dc5c8c610e16017920bb0f2a318505f784
GravleyCM <GravleyCM@gmail.com>
2022-03-06 12:13:53 -0500

    make create function in artist controller

--- controllers/artists.js
+++ controllers/artists.js
@@ -11,7 +11,19 @@ function index(req, res) {
   .catch(err => console.log(err))
 }
 
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+	req.body.tasty = !!req.body.tasty
+  Artist.create(req.body)
+  .then(artist => {
+    res.redirect('/artists')
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
 
 export {
-  index
+  index,
+  create,
 }
