d4de5b2dc65096b4d71a6e015db22fd149cf3db4
GravleyCM <GravleyCM@gmail.com>
2022-03-07 09:42:19 -0500

    set up profiles controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,1 +1,18 @@
 import { Profile } from "../models/profile.js"
+
+function index(req, res) {
+  Profile.find(modelQuery)
+    .sort("name")
+    .exec(function (err, profiles) {
+      if (err) return next(err)
+      res.render("profiles/index", { 
+        profiles, 
+        name: req.query.name,
+        user: req.user
+      })
+    })
+}
+
+export {
+  index
+}
