beedc1fdf52c77aaa7ef79f75c9b18c650ff63ec
GravleyCM <GravleyCM@gmail.com>
2022-03-07 11:57:47 -0500

    create albumSchema

--- models/artist.js
+++ models/artist.js
@@ -2,13 +2,22 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const albumSchema = new Schema ({
+  albumName: String,
+  song: [songSchema],
+}, {
+  timestamps: true
+})
+
 const artistSchema = new Schema ({
   bandName: String,
   genre: String,
   owner: {
     type: Schema.Types.ObjectId, "ref": "Profile"
   },
-  // album: [albumSchema],
+  album: [albumSchema],
+}, {
+  timestamps: true
 })
 
 const Artist = mongoose.model("Artist", artistSchema)
