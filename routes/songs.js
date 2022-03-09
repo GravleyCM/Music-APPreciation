cb68a7934653771ed4d1b6d14391e0fa57ffce1f
GravleyCM <GravleyCM@gmail.com>
2022-03-08 21:39:39 -0500

    continue testing routes

--- routes/songs.js
+++ routes/songs.js
@@ -4,7 +4,7 @@ import * as songsCtrl from "../controllers/songs.js"
 
 const router = Router()
 
-router.post(":id", isLoggedIn, songsCtrl.addSong)
+router.post("/artists/:id/album/:id", isLoggedIn, songsCtrl.addSong)
 
 
 export {
