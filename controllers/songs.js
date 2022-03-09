08f5d65720e95a2ecaa3e2c57aaec6af0f998e2b
GravleyCM <GravleyCM@gmail.com>
2022-03-09 10:06:28 -0500

    fix addSong function

--- controllers/songs.js
+++ controllers/songs.js
@@ -12,20 +12,32 @@ function index(req, res) {
   .catch(err => console.log(err))
 }
 
-function addSong(res, req) {
+function addSong(req, res) {
   // console.log("X!X!X!X!", req.body)
-  Song.findById(req.params.id)
+  Song.findById(req.body)
   // .then(album => {
   //   console.log(req.body)
   //   album.songs.push(req.body)
   //   album.save()
     .then(() => {
       console.log("TEST")
-      res.redirect(`/artists/${req.params.id}`)
+      res.redirect("artists/songs")
     })
   // })
   .catch(err => console.log(err))
 }
+
+// function addAlbum(req, res) {
+//   Artist.findById(req.params.id)
+//   .then(artist => {
+//     artist.albums.push(req.body)
+//     artist.save()
+//     .then(() => {
+//       res.redirect(`/artists/${req.params.id}`)
+//     })
+//   })
+//   .catch(err => console.log(err))
+// }
 
 export {
   index,
