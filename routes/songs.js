eb283c77132b72a702f8f5e12dd52abe4e2b96d1
GravleyCM <GravleyCM@gmail.com>
2022-03-08 21:28:01 -0500

    test new routes

--- routes/songs.js
+++ routes/songs.js
@@ -4,7 +4,7 @@ import * as songsCtrl from "../controllers/songs.js"
 
 const router = Router()
 
-router.post("/songs", isLoggedIn, songsCtrl.addSong)
+router.post(":id/songs", isLoggedIn, songsCtrl.addSong)
 
 
 export {
