ad17d046ea4865d69dafd1d77ec243ac3e55d8f5
GravleyCM <GravleyCM@gmail.com>
2022-03-08 20:34:52 -0500

    work on add song route

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,12 +1,12 @@
 import { Song } from "../models/song.js"
 
 function addSong(res, req) {
-  console.log(req.body);
-  Song.create(req.body)
-  .then(song => {
-    res.render("artists/album")
-  })
-  .catch(err => console.log(err))
+  console.log("X!X!X!X!", req.params.id)
+  // Song.create(req.body)
+  // .then(song => {
+  //   res.render("artists/album")
+  // })
+  // .catch(err => console.log(err))
 }
 
 export {
